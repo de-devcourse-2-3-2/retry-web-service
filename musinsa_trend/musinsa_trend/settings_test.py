a8560647c5bb97af777c9a23c6783dfddbef33dc
secureWKkim <1rladnjsrud1@gmail.com>
2023-12-31 23:15:56 +0900

    fix: trial3

--- musinsa_trend/musinsa_trend/settings_test.py
+++ musinsa_trend/musinsa_trend/settings_test.py
@@ -1,3 +1,4 @@
 # from .settings import DATABASES  # for local
-from musinsa_trend.settings import DATABASES  # 이거 없으면 희한하게 계속 에러남. for linux
+# from musinsa_trend.settings import DATABASES  # 이거 없으면 희한하게 계속 에러남. for linux
+from musinsa_trend.musinsa_trend.settings import DATABASES
 TEST_RUNNER = 'musinsa_trend.test_runner.TestRunner'
