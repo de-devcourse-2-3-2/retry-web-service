35aa59bbc72d533794809bc2a1c05cfe80e52911
secureWKkim <1rladnjsrud1@gmail.com>
2023-12-31 23:59:21 +0900

    feat: (not working) docker test environment

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM python:3.11
 WORKDIR /retry-web-service
-COPY ./crawling /retry-web-service/crawling
+#COPY ./crawling /retry-web-service/crawling
 COPY ./musinsa_trend /retry-web-service/musinsa_trend
 COPY ./requirements.txt /retry-web-service
 COPY ./logger.py /retry-web-service
+COPY ./data/sqlite3 /retry-web-service
 RUN apt-get update
 RUN python -m venv venv
 RUN . venv/bin/activate
@@ -11,5 +12,7 @@ RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 WORKDIR /retry-web-service/musinsa_trend
 #CMD ["python3", "manage.py", "runserver", "localhost:8000"]
-CMD ["python3", "manage.py", "migrate"]
+#CMD ["python3", "manage.py", "migrate"]
+#CMD ["python manage.py migrate && python manage.py runserver 0.0.0.0:8000"]
+CMD ["bash", "-c", "python manage.py migrate && python manage.py test outfits.tests --settings='musinsa_trend.settings_test'"]
 #EXPOSE 8000
--- musinsa_trend/musinsa_trend/test_runner.py
+++ musinsa_trend/musinsa_trend/test_runner.py
@@ -5,15 +5,19 @@ import sqlite3
 class TestRunner(DiscoverRunner):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.sqlite_conn = sqlite3.connect(
-            "/Users/wonkyungkim/Documents/pythondev/retry-web-service/data/sqlite3/musinsa.db")
+        try:
+            self.sqlite_conn = sqlite3.connect("/Users/wonkyungkim/Documents/pythondev/retry-web-service/data/sqlite3/musinsa.db")
+        except:
+            self.sqlite_conn = sqlite3.connect("musinsa.db")
         self.test_DB_cursor = self.sqlite_conn.cursor()
 
     # Creates the test databases by calling setup_databases()
     def setup_databases(self, **kwargs):
-        self.sqlite_conn = sqlite3.connect(
-            # "/Users/wonkyungkim/Documents/pythondev/retry-web-service/musinsa_trend/db.sqlite3")
-            "/Users/wonkyungkim/Documents/pythondev/retry-web-service/data/sqlite3/musinsa.db")
+        try:
+            self.sqlite_conn = sqlite3.connect(
+                "/Users/wonkyungkim/Documents/pythondev/retry-web-service/data/sqlite3/musinsa.db")
+        except:
+            self.sqlite_conn = sqlite3.connect("musinsa.db")
         self.test_DB_cursor = self.sqlite_conn.cursor()
 
     # Destroys the test databases, restoring pre-test conditions by calling teardown_databases()
